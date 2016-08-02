3aa093b7ab46ded2ebdf50b301abd9ecc9bf0dd1
kanonmelodis <kanonmelodis@gmail.com>
2016-08-02 14:20:33 +0900

    support for plus2
    update support for yotogi level, skill and part of trophy in Plus Act.2 plugin.

---
+++ en.js
@@ -0,0 +1,260 @@
+var i18n = {
+	min: "Minimum",
+	max: "Maximum",
+	value: "Current Value",
+
+	game: {
+		profile: "Profile",
+		playerParam: "Player Parameter",
+		summary: "Summary",
+		comment: "Comments",
+
+		maidCount: "# of Maids",
+		saveTime: "Save Time",
+
+		playerName: "Player's Name",
+		gameDays: "# of Days",
+		phaseDays: "Days Within Current Phase",
+		shopUseMoney: "Total CR Spent",
+		money: "Current CR",
+		initSalonLoan: "Initial Loan",
+		salonLoan: "Current Loan",
+		clean: "Clean",
+		beauty: "Beauty",
+		evaluation: "Total Guest Rating",
+		currentSalonGrade: "Current Club Rating",
+		bestSalonGrade: "Best Club Rating",
+
+		stage: {
+			_: "Scenario Phase",
+			loan: "Returning Loan",
+			main: "Main",
+		},
+
+		sexual_mouth: "Erogenous Zone (Mouth)",
+		sexual_throat: "Erogenous Zone (Throat)",
+		sexual_nipple: "Erogenous Zone (Nipple)",
+		sexual_curi: "Erogenous Zone (Clitoris)",
+
+		feature: {
+			energeticSmile: 'Energetic Smile',
+			cheerfulCharm: 'Cheerful Charm',
+			graceful: 'Graceful',
+			pure: 'Pure',
+			angelic: 'Angelic',
+			charming: 'Charming',
+			matureAppeal: 'Mature Appeal',
+			devilishCharm: 'Devilish Charm',
+			dignified: 'Dignified',
+			noble: 'Noble',
+			bashful: 'Bashful',
+			meek: 'Meek',
+			bewitchingCharm: 'Bewitching Charm',
+			elegant: 'Elegant',
+			refined: 'Refined',
+			completelyDevoted: 'Completely Devoted',
+			servant: 'Servant',
+			secretPast: 'Secret Past',
+			fatigue: 'Fatigue',
+			pastIndiscretion: 'Past Indiscretion',
+		},
+		propensity: {
+			licentious: "Licentious",
+			m: "Masochist Maid",
+			anal: "Loves Anal",
+			twohole: "Loves 2 Hole",
+			hentai: "Hentai",
+			service: "Loves Service",
+			creampie: "Loves Creampie",
+			drink: "Loves Drinking Semen",
+		},
+
+		seikenken: {
+			none: "None",
+			front: "Front",
+			back: "Back",
+			both: "Both"
+		},
+
+		maid: {
+			lastName: "Last Name",
+			firstName: "First Name",
+			callByName: "Call by Name",
+
+			contractType: "Contract Type",
+			nurture: "Trainee",
+			freeMaid: "Free Maid",
+			privateMaid: "Private Maid",
+
+			isRented: "Rented Maid",
+
+			personality: "Personality",
+			p_innocent: "Innocent",
+			p_cool: "Cool",
+			p_proud: "Tsundere",
+			p_yandere: "Yandere",
+			p_onnechan: "Onee-chan",
+
+			initSeikenken: "Initial Sex Experience",
+			seikenken: "Sex Experience",
+
+			condition: "Condition",
+			c_nervous: "Nervous",
+			c_acquaintance: "Acquaintance",
+			c_trust: "Trust",
+			c_lover: "Lover",
+			c_slave: "Slave",
+
+			yotogiPlayCount: "# of Night Services",
+			othersPlayCount: "# of Entertainments",
+
+			hp: "Stamina",
+			reason: "Reason",
+			mind: "Mind",
+			likability: "Favor",
+			care: "Care",
+			reception: "Reception",
+			studyRate: "Sutdy Rate",
+			teachRate: "Leadership",
+			lovely: "Sweetness",
+			elegance: "Elegance",
+			charm: "Charm",
+			inyoku: "Lust",
+			mValue: "Masochism",
+			hentai: "Lewdness",
+			housi: "Service"
+		},
+
+		body: {
+			head: "Head",
+			eyeSize: "Size of Eyes",
+			eyeX: "Eye X",
+			eyeY: "Eye Y",
+			headW: "Head Width",
+			headH: "Head Height",
+
+			whole: "Whole",
+			legL: "Leg Length",
+			bodyL: "Body Length",
+
+			breast: "Breast",
+			breastSize: "Breast Size",
+			breastSagging: "Breast Sagging",
+			breastUpDown: "Breasts' Vertical Position",
+			breastDevi: "Breasts' Deviation",
+
+			upper: "Upper-half",
+			waist: "Waist",
+			belly: "Belly",
+			shoulder: "Shoulder Width",
+			armT: "Arm Thickness",
+			armL: "Arm Length",
+			neckL: "Neck Length",
+
+			lower: "Lower-half",
+			butt: "Butt",
+			legFat: "Leg Fatness",
+			legMuscle: "Leg Muscle",
+		}
+	},
+
+	createurl: "",
+	invalidsave: "Invalid CM3D2 Save",
+	invalidjson: "Invalid JSON",
+
+	bodyEditor: "Body Editor",
+	featureEditor: "Feature Editor",
+	propensityEditor: "Propensity Editor",
+	pleaseSelect: "Please Select",
+
+	utility: "Utilities",
+
+	ui: {
+		load: "Load",
+		save: "Save",
+		loadjson: "Load JSON",
+		savejson: "Save JSON",
+		setting: "Settings",
+
+		close: "Close",
+
+		editBody: "Edit Body",
+		editFeature: "Edit Feature",
+		editPropensity: "Edit Propensity",
+
+		updateNotice: "CM3D2 Editor is updated to version ${version}",
+		updateHistoryTemplate: "Update History<br/>${body}",
+
+		aboutTitle: "About",
+		aboutTemplate: "Author: MainVirtualizer<br/>GitHub Repo: <a href=\"https://github.com/MainVirtualizer/CM3D2Editor\">https://github.com/MainVirtualizer/CM3D2Editor</a><br/>Report Issues: <a href=\"https://github.com/MainVirtualizer/CM3D2Editor/issues\">https://github.com/MainVirtualizer/CM3D2Editor/issues</a><br/><br/>${history}",
+	},
+
+	setting: {
+		title: "Settings",
+		ytgc001: "I have installed Yotogi Class Skills Vol.1",
+		cbp: "I have installed Chu-B Lip Special Version",
+		ytgc002: "I have installed Yotogi Class Skills Vol.2",
+		ytgc003: "I have installed Yotogi Class Skills Vol.3",
+		yandere: "I have installed Yandere Personality Pack",
+		dkg_winter: "I have installed Denkigai 2015 Winter",
+		dkg_winter_panties: "I have installed Denkigai 2015 Winter Panties",
+		plus: "I have installed Plus Pack",
+		ytgc004: "I have installed Yotogi Class Skills Vol.4",
+		ytgc005: "I have installed Yotogi Class Skills Vol.5",
+		oneechan: "I have installed Onee-chan Personality Pack",
+		ytgc006: "I have installed Yotogi Class Skills Vol.6",
+		ytgc007: "I have installed Yotogi Class Skills Vol.7",
+		plus2: "I have installed Plus ACT.2 Pack"
+	},
+
+	util: {
+		unlockBodyLimits: "Unlock All Maids' Height Limits",
+		unlockBodyLimitsFinished: "All maids' height limits are unlocked",
+		allItems: "Unlock All Items",
+		allItemsFinished: "All items are unlocked",
+		allTrophies: "Unlock All Trophies",
+		allTrophiesFinished: "All trophies are unlocked",
+		maidClassMax: "Maximize Current Maid's Maid Class",
+		maidClassMaxFinished: "Current maid's maid class is maximized",
+		yotogiClassMax: "Maximize Current Maid's Yotogi Class",
+		yotogiClassMaxFinished: "Current maid's yotogi class is maximized",
+		allSkills: "Master All Skills For Current Maid",
+		allSkillsFinished: "Current maid now masters all skills",
+		allWork: "Master All Works For Current Maid",
+		allWorkFinished: "Current maid now masters all works",
+		masterPlayedSkills: "Master Played Skills For Selected Maid",
+		masterPlayedSkillsFinished: "Selected maid now masters all played skills",
+		removeExGrpVIP: "Remove Exchange, Group and VIP Experiences",
+		removeExGrpVIPFinished: "Exchange, group and VIP experiences removed",
+		removeModItems: "Remove All Mod Items",
+		removeModItemsFinished: "${count} mod item(s) removed",
+		finishAllVip: "Mark All VIP Events As Finished",
+		finishAllVipDone: "All VIP events are marked as finished"
+	},
+
+	updateHistory: {
+		"1.7.2": ["CM3D2 1.32 Tested", "Yotogi Skills Vol.7 Supported"],
+		"1.7.1": ["CM3D2 1.31 Tested", "Yotogi Skills Vol.6 Supported"],
+		"1.7.0": ["CM3D2 1.30 Tested", "One-chan Personality Pack Supported", "Denkigai 2015 Winter Panties Supported"],
+		"1.6.3": ["CM3D2 1.27 Tested", "Yotogi Skills Vol.5 Supported"],
+		"1.6.2": ["CM3D2 1.26 Tested", "Yotogi Skills Vol.4 Supported"],
+		"1.6.1": ["Utility Added: Mark All VIP Events As Finished", "<small>So you can replay the event.</small>"],
+		"1.6.0": ["CM3D2 1.22 Tested", "Plus Pack Supported"],
+		"1.5.2": ["Denkigai 2015 Winter Supported"],
+		"1.5.1": ["Yandere Personality Pack Supported"],
+		"1.5.0": ["Yotogi Skills Vol.3 Supported"],
+		"1.4.5": ["CM3D2 1.19 Supported"],
+		"1.4.4": ["Yotogi Skills Vol.2 Supported"],
+		"1.4.3": ["Chu-B Skills Supported"],
+		"1.4.2": ["New DLC Yotogi Skills Supported", "Enable DLC Features in Setting"],
+		"1.4.1": ["CM3D2 1.10 Tested", "New DLC Yotogi Class Supported"],
+		"1.4.0": ["Utility Added: Remove All Mod Items", "<small>This can fix hanging of the game when mods are removed</small>"],
+		"1.3.0": ["CM3D2 1.09 Tested", "Utility Added: Remove Exchange, Group and VIP Experiences"],
+		"1.2.1": ["CM3D2 1.05 Tested"],
+		"1.2.0": ["Utility Added: Master Only Played Skills For Selected Maid"],
+		"1.1.2": ["Add IE 10 Support"],
+		"1.1.1": ["Add English Translation"],
+		"0.0.1-1.1.0": ["Basic Functionalities"]
+	}
+
+};
---
+++ main.js
@@ -0,0 +1,573 @@
+'use strict';
+
+var openedFileName;
+var bindings = {
+	showMaidUtil: false,
+	version: "1.7.2",
+	msgbox: {
+		title: '',
+		text: ''
+	}
+};
+
+function showMsgbox(title, text) {
+	bindings.msgbox.title = title;
+	bindings.msgbox.text = text;
+	$('#message').openModal();
+}
+
+function updateMaterialSelect(obj) {
+	obj.material_select('update');
+	obj.closest('.input-field').children('span.caret').remove();
+}
+
+function updateMaterialize() {
+	$("input").change();
+	$("textarea").change().keydown();
+	updateMaterialSelect($('select'));
+	setTimeout(function() {
+		$('ul.tabs').tabs();
+	}, 300);
+}
+
+function openOpenDialog(callback, accept) {
+	var fileSelector = $('<input type="file">');
+	if (accept) fileSelector.attr('accept', accept);
+	fileSelector.change(function() {
+		var files = fileSelector[0].files;
+		if (files.length) {
+			callback(files[0]);
+		} else {
+			callback(null);
+		}
+	});
+	fileSelector.click();
+}
+
+function createDataURL(data) {
+	var base64;
+	if (data instanceof ArrayBuffer) {
+		data = new Uint8Array(data);
+	}
+	if (data instanceof Uint8Array) {
+		base64 = data.toBase64();
+	} else {
+		base64 = btoa(unescape(encodeURIComponent(data)));
+	}
+	return 'data:application/octet-stream;base64,' + base64;
+}
+
+$('#button-upload').click(function() {
+	openOpenDialog(function(file) {
+		if (file) {
+			var reader = new FileReader();
+			reader.onload = function() {
+				try {
+					loadJSON(parseSaveData(reader.result));
+				} catch (e) {
+					alert(i18n.invalidsave + ' ' + e.message);
+				}
+				openedFileName = file.name.replace(/\.[^.]+$/, '');
+			}
+			reader.readAsArrayBuffer(file);
+		}
+	}, '.save');
+});
+
+$('#button-loadjson').click(function() {
+	openOpenDialog(function(file) {
+		if (file) {
+			var reader = new FileReader();
+			reader.onload = function() {
+				try {
+					loadJSON(JSON.parse(reader.result));
+				} catch (e) {
+					alert(i18n.invalidjson);
+				}
+				openedFileName = file.name.replace(/\.[^.]+$/, '');
+			}
+			reader.readAsText(file);
+		}
+	}, '.json');
+});
+
+$('#button-download').click(function() {
+	saveAs(new Blob([new Uint8Array(writeSaveData(bindings.save))]), openedFileName + '.save');
+});
+
+$('#button-savejson').click(function() {
+	saveTextAs(JSON.stringify(bindings.save, null, 2), openedFileName + '.json');
+});
+
+$('#button-createurl').click(function() {
+	var newWindow = window.open();
+	newWindow.document.body.innerHTML = '如果安装了迅雷 请用右键另存为<br/><a download="' + openedFileName + '.save" href="' + createDataURL(writeSaveData(bindings.save)) + '">CM3D2 Save文件格式</a><br/>' + '<a download="' + openedFileName + '.json" href="' + createDataURL(JSON.stringify(bindings.save, null, 2)) + '">JSON文件格式</a>';
+});
+
+$('#button-settings').click(function() {
+	$('#settings').openModal();
+});
+
+
+function loadJSON(model) {
+	bindings.save = model;
+
+	// Short path
+	bindings.header = model.header;
+	bindings.player = model.chrMgr.playerParam;
+
+	// Select first tab
+	$('a[href=#profile]').click();
+
+	// Select first maid
+	$($('a[maid-guid]')[0]).click();
+
+	$('#saveEditor').show(200);
+	updateMaterialize();
+}
+
+function limitNum(value, min, max) {
+	value = parseInt(value) || 0;
+	if (value < min) return min;
+	if (value > max) return max;
+	return value;
+}
+
+rivets.formatters.int32 = {
+	read: function(value) {
+		return value;
+	},
+	publish: function(value) {
+		return parseInt(value) || 0;
+	}
+};
+
+rivets.formatters.int64 = {
+	read: function(value) {
+		if (value)
+			return new Long(value[0], value[1]).toString();
+		return '';
+	},
+	publish: function(value) {
+		try {
+			var long = Long.fromString(value);
+		} catch (e) {
+			return [0, 0];
+		}
+		return [long.low_, long.high_];
+	}
+};
+
+rivets.formatters.rate = {
+	read: function(value) {
+		return value / 10;
+	},
+	publish: function(value) {
+		value = parseFloat(value) || 0;
+		return Math.round(value * 10);
+	}
+};
+
+rivets.formatters._9999 = {
+	read: function(value) {
+		return value;
+	},
+	publish: function(value) {
+		return limitNum(value, 0, 9999);
+	}
+};
+
+rivets.adapters['#'] = {
+	observe: function(obj, keypath, callback) {},
+	unobserve: function(obj, keypath, callback) {},
+	get: function(obj, keypath) {
+		keypath = JSON.parse(keypath);
+		return obj.indexOf(keypath) !== -1;
+	},
+	set: function(obj, keypath, value) {
+		keypath = JSON.parse(keypath);
+		var idx = obj.indexOf(keypath);
+		var existing = idx !== -1;
+		if (existing !== value) {
+			if (value) {
+				obj.push(keypath);
+			} else {
+				obj.splice(idx, 1);
+			}
+		}
+	}
+}
+
+var localStorageChangeCallback = {};
+rivets.adapters['/'] = {
+	observe: function(obj, keypath, callback) {
+		localStorageChangeCallback[keypath] = localStorageChangeCallback[keypath] || [];
+		localStorageChangeCallback[keypath].push(callback);
+	},
+	unobserve: function(obj, keypath, callback) {
+		var obj = localStorageChangeCallback[keypath];
+		if (obj) obj.splice(obj.indexOf(callback), 1);
+	},
+	get: function(obj, keypath) {
+		try {
+			return JSON.parse(localStorage[keypath]);
+		} catch (e) {
+			return undefined;
+		}
+	},
+	set: function(obj, keypath, value) {
+		localStorage[keypath] = JSON.stringify(value);
+		var arrays = localStorageChangeCallback[keypath];
+		if (arrays) arrays.forEach(function(i) {
+			i();
+		});
+		$('#personality').material_select();
+	}
+}
+
+rivets.formatters.image = function(base64) {
+	return 'data:image/png;base64,' + base64;
+}
+
+rivets.formatters.format = function(data, string) {
+	return string.replace('$0', data);
+};
+
+rivets.formatters.eval = function(data, string) {
+	return new Function('$0', 'return ' + string)(data);
+};
+
+$(function() {
+	var templates = $('script[type="text/template"]');
+	for (var i = 0; i < templates.length; i++) {
+		var t = $(templates[i]);
+		t.before(t.text()).remove();
+	}
+});
+
+function getMaidByGUID(guid) {
+	var maids = bindings.save.chrMgr.stockMaid;
+	for (var i = 0; i < maids.length; i++) {
+		if (maids[i].param.guid === guid) return maids[i];
+	}
+	return null;
+}
+
+function selectMaid(guid) {
+	bindings.maid = getMaidByGUID(guid);
+	updateMaterialize();
+}
+
+function editBody(guid) {
+	$('#bodyEditor_selector').val('');
+	bindings.bodyEditor.property = null;
+
+	$('#bodyEditor').openModal();
+};
+
+function editFeature(guid) {
+	$('#featureEditor').openModal();
+};
+
+function editPropensity(guid) {
+	$('#propensityEditor').openModal();
+};
+
+bindings.bodyEditor = {
+	property: null
+};
+
+var bodyEditor = {
+	changeProperty: function(value) {
+		bindings.bodyEditor.property = bindings.maid.props[value];
+		updateMaterialize();
+	}
+};
+
+var util = {
+	unlockBodyLimits: function() {
+		var allMaids = bindings.save.chrMgr.stockMaid;
+		for (var i = 0; i < allMaids.length; i++) {
+			var maid = allMaids[i];
+			maid.props.DouPer.min = 0;
+			maid.props.DouPer.max = 100;
+			maid.props.sintyou.min = 0;
+			maid.props.sintyou.max = 100;
+		}
+		Materialize.toast(i18n.util.unlockBodyLimitsFinished, 4000);
+	},
+	allItems: function() {
+		var list = bindings.player.haveItemList;
+		var keys = Object.getOwnPropertyNames(list);
+		for (var i = 0; i < keys.length; i++) {
+			if (list.propertyIsEnumerable(keys[i])) {
+				list[keys[i]] = true;
+			}
+		}
+		Materialize.toast(i18n.util.allItemsFinished, 4000);
+	},
+	allTrophies: function() {
+		bindings.player.haveTrophyList = [
+			1, 2, 3, 4, 5, 6, 7, 8, 9,
+			10, 20, 30, 40, 50, 60, 70, 80, 90,
+			100, 110, 120, 130, 140, 150, 160, 170, 180, 190,   155, 192, 194,
+			200, 210, 220, 230, 240, 250, 260, 270, 280, 290,   202, 222,  232 ,234, 236, 238,
+			300, 310, 320, 330, 340, 350, 360, 370, 380, 390,
+			400, 410, 420, 430, 440, 450, 460, 470, 480, 490,
+			500, 510, 520, 530, 540, 550, 560, 570, 580, 590,
+			600, 610, 620, 630, 640, 650, 660, 670, 680, 690,
+			700
+		];
+		Materialize.toast(i18n.util.allTrophiesFinished, 4000);
+	},
+	maidClassMax: function() {
+		var data = bindings.maid.param.maidClassData;
+		var totalExp = [320, 395, 395, 395, 500, 500, 500];
+		for (var i = 0; i < 7; i++) {
+			data[i].have = true;
+			data[i].exp.currentExp = 0;
+			data[i].exp.level = 10;
+			data[i].exp.nextExp = 0;
+			data[i].exp.totalExp = totalExp[i];
+		}
+		Materialize.toast(i18n.util.maidClassMaxFinished, 4000);
+	},
+	yotogiClassMax: function() {
+		var data = bindings.maid.param.yotogiClassData;
+		var classes = [0, 1, 2, 3, 4, 5, 6];
+		if (localStorage.ytgc001 === "true") classes.push(7);
+		if (localStorage.ytgc002 === "true") classes.push(8);
+		if (localStorage.ytgc003 === "true") classes.push(9);
+		if (localStorage.dkg_winter === "true") classes.push(10);
+		if (localStorage.plus === "true") classes.push(11, 12, 13, 14);
+		if (localStorage.ytgc004 === "true") classes.push(15);
+		if (localStorage.ytgc005 === "true") classes.push(16);
+		if (localStorage.ytgc006 === "true") classes.push(17);
+		if (localStorage.ytgc007 === "true") classes.push(18);
+		if(localStorage.plus2==="true") classes.push(19, 20, 21, 22);
+		for (var i = 0; i < classes.length; i++) {
+			var idx = classes[i];
+			data[idx].have = true;
+			data[idx].exp.currentExp = 0;
+			data[idx].exp.level = 10;
+			data[idx].exp.nextExp = 0;
+			data[idx].exp.totalExp = 4530;
+		}
+		Materialize.toast(i18n.util.yotogiClassMaxFinished, 4000);
+	},
+	allSkills: function() {
+		var data = bindings.maid.param.skillData;
+		var skillIndex = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150, 160, 170, 180, 190, 200, 210, 220, 230, 240, 250, 260, 270, 280, 290, 300, 310, 320, 330, 340, 345, 350, 360, 370, 380, 390, 400, 410, 420, 430, 440, 450, 460, 470, 480, 490, 500, 510, 520, 530, 540, 550, 560, 570, 580, 590, 600, 610, 620, 630, 640, 650, 660, 670, 680, 690, 700, 710, 720, 730, 740, 750, 760, 770, 780, 790, 800, 810, 820, 830, 840, 850, 860, 870, 880, 890, 900, 910, 920, 930, 940, 950, 960, 970, 980, 990, 1000];
+		if (localStorage.ytgc001 === "true") {
+			skillIndex.push(1040, 1050, 1060, 1070, 1080, 1090, 1100, 1110);
+		}
+		if (localStorage.ytgc002 === "true") {
+			skillIndex.push(1120, 1130, 1140, 1150, 1160, 1170, 1180, 1190);
+		}
+		if (localStorage.ytgc003 === "true") {
+			skillIndex.push(1200, 1210, 1220, 1230, 1240, 1250, 1260, 1270, 1280);
+		}
+		if (localStorage.cbp === "true") {
+			skillIndex.push(15, 25, 35, 55, 75, 95, 115, 135, 155, 175, 195, 225, 235, 245, 255, 265, 275, 285, 295, 305, 335, 347, 365, 375, 395, 405, 415, 425, 435, 445, 475, 485, 505, 515, 535, 545, 555, 575, 585, 595, 625, 645, 655, 665, 675, 695, 705, 715, 725, 755, 765, 1010, 1020);
+		}
+		if (localStorage.dkg_winter === 'true') {
+			skillIndex.push(1290, 1300, 1310, 1320);
+		}
+		if (localStorage.dkg_winter_panties === 'true') {
+			skillIndex.push(1330);
+		}
+		if (localStorage.plus === 'true') {
+			for (var i = 1340; i <= 1510; i += 10) {
+				skillIndex.push(i);
+			}
+		}
+		if (localStorage.ytgc004 === 'true') {
+			for (var i = 1520; i <= 1590; i += 10) {
+				skillIndex.push(i);
+			}
+		}
+		if (localStorage.ytgc005 === 'true') {
+			for (var i = 1600; i <= 1680; i += 10) {
+				skillIndex.push(i);
+			}
+		}
+		if (localStorage.oneechan === 'true') {
+			skillIndex.push(1690, 1700);
+		}
+		if (localStorage.ytgc006 === 'true') {
+			for (var i = 1710; i <= 1770; i += 10) {
+				skillIndex.push(i);
+			}
+		}
+		if (localStorage.ytgc007 === 'true') {
+			for (var i = 1790; i <= 1870; i += 10) {
+				skillIndex.push(i);
+			}
+		}
+		if (localStorage.plus2 === 'true') {
+			for (var i = 1880; i <= 2260; i += 10) {
+				skillIndex.push(i);
+			}
+		}
+		for (var i = 0; i < skillIndex.length; i++) {
+			var idx = skillIndex[i];
+			if (data[idx]) {
+				data[idx].exp.currentExp = 0;
+				data[idx].exp.level = 3;
+				data[idx].exp.nextExp = 0;
+				data[idx].exp.totalExp = 300;
+			} else {
+				data[idx] = {
+					exp: {
+						currentExp: 0,
+						level: 3,
+						nextExp: 0,
+						totalExp: 300
+					},
+					id: idx,
+					playCount: 0
+				};
+			}
+		}
+		Materialize.toast(i18n.util.allSkillsFinished, 4000);
+	},
+	allWork: function() {
+		var data = bindings.maid.param.workData;
+		var workIndex = [1, 2, 3, 4, 5, 6, 7, 8, 9, 101, 1001, 1002];
+		for (var i = 0; i < workIndex.length; i++) {
+			var idx = workIndex[i];
+			if (data[idx]) {
+				data[idx].level = 3;
+				data[idx].playCount = 20;
+			} else {
+				data[idx] = {
+					id: idx,
+					level: 3,
+					playCount: 20
+				};
+			}
+		}
+		Materialize.toast(i18n.util.allWorkFinished, 4000);
+	},
+	masterPlayedSkills: function() {
+		var data = bindings.maid.param.skillData;
+		var skillIndex = Object.getOwnPropertyNames(data);
+		for (var i = 0; i < skillIndex.length; i++) {
+			var idx = skillIndex[i];
+			if (data.propertyIsEnumerable(idx) && data[idx].playCount) {
+				data[idx].exp.currentExp = 0;
+				data[idx].exp.level = 3;
+				data[idx].exp.nextExp = 0;
+				data[idx].exp.totalExp = 300;
+			}
+		}
+		Materialize.toast(i18n.util.masterPlayedSkillsFinished, 4000);
+	},
+	removeExGrpVIP: function() {
+		bindings.maid.param.genericFlag.夜伽_カテゴリー_実行回数_乱交 = 0;
+		bindings.maid.param.genericFlag.夜伽_カテゴリー_実行回数_交換 = 0;
+		bindings.maid.param.genericFlag._PlayedNightWorkVip = 0;
+		Materialize.toast(i18n.util.removeExGrpVIPFinished, 4000);
+	},
+	removeModItems: function() {
+		var maids = bindings.save.chrMgr.stockMaid;
+		var count = 0;
+		for (var i = 0; i < maids.length; i++) {
+			forEachKeys(maids[i].props, function(prop, key, value) {
+				if (value.fileName.indexOf(".mod") !== -1) {
+					count++;
+					delete prop[key];
+				}
+			});
+		}
+		Materialize.toast(i18n.util.removeModItemsFinished.replace(/\$\{count\}/g, count), 4000);
+	},
+	finishAllVip: function() {
+		forEachKeys(bindings.save.chrMgr.playerParam.nightWorksStateDic, function(prop, key, value) {
+			value.finish = true;
+		});
+		Materialize.toast(i18n.util.finishAllVipDone, 4000);
+	}
+};
+
+$(document).ready(function() {
+	rivets.bind($('body'), i18n, {
+		prefix: 'i18n',
+		templateDelimiters: ['[', ']'],
+	});
+	rivets.bind($('body'), bindings, {
+		prefix: 'bind'
+	});
+	$('select').material_select();
+});
+
+
+function forEachKeys(obj, callback) {
+	var keys = Object.getOwnPropertyNames(obj);
+	for (var i = 0; i < keys.length; i++) {
+		if (obj.propertyIsEnumerable(keys[i])) {
+			callback(obj, keys[i], obj[keys[i]]);
+		}
+	}
+}
+
+function showChangeLog() {
+	var title = i18n.ui.updateNotice.replace('${version}', bindings.version);
+	var body = '';
+	forEachKeys(i18n.updateHistory, function(obj, key, value) {
+		body += key + '<br/>' + value.map(function(a) {
+			return '&emsp;&emsp;' + a + '<br/>';
+		}).join('');
+	});
+	showMsgbox(
+		title,
+		i18n.ui.updateHistoryTemplate.replace('${body}', body)
+	);
+}
+
+function checkVersion() {
+	if (localStorage.version !== bindings.version) {
+		localStorage.version = bindings.version;
+		showChangeLog();
+	}
+}
+
+function showAbout() {
+	var body = '';
+	forEachKeys(i18n.updateHistory, function(obj, key, value) {
+		body += key + '<br/>' + value.map(function(a) {
+			return '&emsp;&emsp;' + a + '<br/>';
+		}).join('');
+	});
+	showMsgbox(
+		i18n.ui.aboutTitle,
+		i18n.ui.aboutTemplate.replace('${history}', body)
+	);
+}
+
+function include(file, callback) {
+	var script = document.createElement('script');
+	script.src = file;
+	script.onload = function() {
+		document.head.removeChild(script);
+		callback && callback();
+	};
+	document.head.appendChild(script);
+}
+
+function switchLocale(locale, callback) {
+	var oldi18n = i18n;
+	include('i18n/' + locale + '.js', function() {
+		var newi18n = i18n;
+		i18n = oldi18n;
+		$.extend(oldi18n, newi18n);
+		callback && callback();
+	});
+}
+
+var locale = localStorage.locale || navigator.language || navigator.browserLanguage;
+if (locale.indexOf("zh") !== -1) {
+	switchLocale('zh', checkVersion);
+} else {
+	checkVersion();
+}
